1c75a0469748ef13e13239f4325c850710d6c12d
Mike <102953584+michelangelodorado@users.noreply.github.com>
2024-10-07 22:49:06 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -160,23 +160,23 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 
 # Create the Node Group in the new VPC subnets
 resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.kube-tf-eks.name
+  cluster_name   = aws_eks_cluster.kube-tf-eks.name
   node_group_name = "kube-tf-workernodes"
   node_role_arn  = aws_iam_role.workernodes.arn
-  subnet_ids   = [
+  subnet_ids     = [
     aws_subnet.subnet_a.id,
     aws_subnet.subnet_b.id
   ]
   instance_types = ["t3.small"]
 
   scaling_config {
-   desired_size = 1
-   max_size   = 1
-   min_size   = 1
+    desired_size = var.worker_nodes_count
+    max_size     = var.worker_nodes_count
+    min_size     = var.worker_nodes_count
   }
 
   depends_on = [
-   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
   ]
 }
